c3a23d0d912dbf003b4badad007ac444c19e71e5
Francois van Rensburg <jvrensburgfrans@gmail.com>
2020-04-09 11:57:36 +0200

    Updates

---
+++ precache-manifest.f8aa3ae9582b87530cdd663cd2fd8fde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bf71799ccd74e5a81c1879e4b1f9125",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "27ee14a8e5fac48c488e",
+    "url": "/monsters-rolodex/static/css/main.4eaf69d4.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ee14a8e5fac48c488e",
+    "url": "/monsters-rolodex/static/js/main.39b30399.chunk.js"
+  },
+  {
+    "revision": "83fef09b6b4b91e8e383",
+    "url": "/monsters-rolodex/static/js/runtime-main.6036d2a0.js"
+  }
+]);
